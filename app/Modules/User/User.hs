140b257a697d378446b1dbe31876b6aad113357c
igoramf <igoraaf1@gmail.com>
2023-09-14 09:56:58 -0300

    feat: Funcionalidades de User

--- app/Modules/User/User.hs
+++ app/Modules/User/User.hs
@@ -1,6 +1,17 @@
 module Modules.Users.Users where
-import System.IO
 
 
-data User = User { username :: String, name :: String, password :: String, isAdm :: Bool }
+data User = User { username :: String, name :: String, password :: String, isAdm :: Bool } deriving (Show)
 
+
+getUserName :: User -> String
+getUserName = username
+
+getName :: User -> String
+getName = name
+
+getPassword :: User -> String
+getPassword = password
+
+checkUserType :: User -> Bool
+checkUserType = isAdm
