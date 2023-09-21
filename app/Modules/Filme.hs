949e64d0b33fb06f9600bf42a28026afb5c6e6ec
Ivan Bruno Abrantes de Oliveira <78627914+Ivan-Bruno@users.noreply.github.com>
2023-09-20 22:28:05 -0300

    Update Filme.hs

--- app/Modules/Filme.hs
+++ app/Modules/Filme.hs
@@ -1,17 +1,18 @@
---Filme tem id, titulo, preço, sala e duração
---atualizar preço de um Filme permitindo que descontos sejam aplicados
---visualizar lista de Filmes (Filmes existentes no cinema e Filmes de acordo com as sala)
+--filme tem id, nome, preço, salas e tempo de duracao
+--atualizar preço de um filme permitindo que descontos sejam aplicados
+--visualizar lista de filmes (filmes existentes no cinema e filmes de acordo com as salas)
 
-module Filme (getFilmePeloTitulo, getTituloFilme, fromIO, converteSalaEmLista, getFilmesEmLista,
-    getSalaFilme,
-    getSalaFilmeToString,
+module Filme (getFilmePeloId, getIdFilme, fromIO, converteSalasEmLista, getFilmesEmLista,
+    getSalasFilme,
+    getSalasFilmeToString,
     filmeToString,
-    getSalaFilmes,
+    getSalasFilmes,
     getFilmes,
     formataParaEscrita,
     escreverArquivo,
     converteEmLista,
     setPreco,
+    getFilmesPuros,
     Filmes(Filmes),
     Filme(Filme)
 ) where
@@ -22,50 +23,52 @@ import System.IO.Unsafe
 
 data Filme = Filme {
     idFilme :: Int,
-    tituloFilme :: String,
+    nomeFilme :: String,
     preco :: Double,
-    salaFilme :: [String],
+    salasFilme :: [String],
     duracao :: String
 } deriving (Show, Read)
 
+
+
 data Filmes = Filmes {
-    Filmes :: [(Int, Filme)]
+    filmes :: [(Int, Filme)]
 } deriving Show
 
 
-----------------------------FilmeGetters--------------------
+----------------------------PRODUTOGetters--------------------
 
 getFilmes :: Filmes -> [Filme]
-getFilmes (Filmes {Filmes = p}) = getFilmesFromTuple p
+getFilmes (Filmes {filmes = p}) = getFilmesFromTuple p
 
 getFilmesFromTuple :: [(Int, Filme)] -> [Filme]
 getFilmesFromTuple [] = []
 getFilmesFromTuple ((_,c): cs) = c : getFilmesFromTuple cs
 
-getTituloFilme :: Filme -> Int
-getTituloFilme Filme {idFilme = i} = i
+getIdFilme :: Filme -> Int
+getIdFilme Filme {idFilme = i} = i
 
-getTituloFilme :: Filme -> String
-getTituloFilme Filme {tituloFilme = n} = n
+getNomeFilme :: Filme -> String
+getNomeFilme Filme {nomeFilme = n} = n
 
 getPreco :: Filme -> Double
 getPreco Filme {preco = p} = p 
 
-getFilmePeloTitulo :: Int -> [Filme] -> Maybe Filme
-getFilmePeloTitulo id [] = Nothing
-getFilmePeloTitulo id (p:ps) = if id == getTituloFilme p then Just p
-    else getFilmePeloTitulo id ps
+getFilmePeloId :: Int -> [Filme] -> Maybe Filme
+getFilmePeloId id [] = Nothing
+getFilmePeloId id (p:ps) = if id == getIdFilme p then Just p
+    else getFilmePeloId id ps
 
-getSalaFilme :: Filme -> [String]
-getSalaFilme Filme {salaFilme = s} = s
+getSalasFilme :: Filme -> [String]
+getSalasFilme Filme {salasFilme = s} = s
 
-getSalaFilmes :: [Filme] -> String
-getSalaFilmes [] = []
-getSalaFilmes (c:cs) = show (getTituloFilme c) ++ "," ++ getSalaFilmeToString (getSalaFilme c) ++ getSalaFilmes cs
+getSalasFilmes :: [Filme] -> String
+getSalasFilmes [] = []
+getSalasFilmes (c:cs) = show (getIdFilme c) ++ "," ++ getSalasFilmeToString (getSalasFilme c) ++ getSalasFilmes cs
 
-getSalaFilmeToString :: [String] -> String
-getSalaFilmeToString [] = []
-getSalaFilmeToString (c:cs) = if length cs > 0 then c ++ "," ++ getSalaFilmeToString cs else c ++ "\n"
+getSalasFilmeToString :: [String] -> String
+getSalasFilmeToString [] = []
+getSalasFilmeToString (c:cs) = if length cs > 0 then c ++ "," ++ getSalasFilmeToString cs else c ++ "\n"
 
 getDuracao :: Filme -> String
 getDuracao Filme {duracao = v} = v
@@ -75,44 +78,44 @@ getDuracao Filme {duracao = v} = v
 setPreco :: [Filme] -> Int -> Double -> [Filme]
 setPreco [] x novoPreco = []
 setPreco (c:cs) x novoPreco
-    | idAtual == x = ([Filme x tituloFilmeAtual novoPreco salaFilmeAtual duracaoAtual] ++ cs)
+    | idAtual == x = ([Filme x nomeFilmeAtual novoPreco salasFilmeAtual duracaoAtual] ++ cs)
     | otherwise = setPreco cs x novoPreco
     where
-        idAtual = getTituloFilme c
-        tituloFilmeAtual = gettituloFilme c
-        salaFilmeAtual = getSalaFilme c
+        idAtual = getIdFilme c
+        nomeFilmeAtual = getNomeFilme c
+        salasFilmeAtual = getSalasFilme c
         duracaoAtual = getDuracao c
 
 
 
 filmeToString :: Filme -> String
-filmeToString Filme {idFilme = i, tituloFilme = n, preco = p, duracao = v} = show i ++"," ++ n ++ "," ++ show p ++ "," ++ v
+filmeToString Filme {idFilme = i, nomeFilme = n, preco = p, duracao = v} = show i ++"," ++ n ++ "," ++ show p ++ "," ++ v
 
 
 
-FilmesToString :: [Filme] -> String
-FilmesToString [] = []
-FilmesToString (p:ps) = if length ps > 0 then do "["++filmeToString p ++"]," ++ FilmesToString ps
+filmesToString :: [Filme] -> String
+filmesToString [] = []
+filmesToString (p:ps) = if length ps > 0 then do "["++filmeToString p ++"]," ++ filmesToString ps
     else do "[" ++ filmeToString p ++ "]"
 
 
 
-salaToString :: [String] -> String
-salaToString [] = []
-salaToString (s:sw) = s ++ salaToString sw
+salasToString :: [String] -> String
+salasToString [] = []
+salasToString (s:sw) = s ++ salasToString sw
 
 -----------------------------IOFilme---------------------------------
 
 escreverArquivo :: [Filme] -> IO ()
-escreverArquivo Filme = do
+escreverArquivo filme = do
     arq <- openFile "../arquivos/Filmes.csv" AppendMode
-    arq1 <- openFile "../arquivos/salaFilme.csv" AppendMode
+    arq1 <- openFile "../arquivos/SalasFilme.csv" AppendMode
     
-    print (Filme) -- mudar isso, mas fazer depois
+    print (filme) -- mudar isso, mas fazer depois
 
-    let datasalaFilme = getSalaFilmes Filme
-    hPutStr arq1 (datasalaFilme)
-    hPutStr arq (formataParaEscrita Filme)
+    let dataSalasFilme = getSalasFilmes filme
+    hPutStr arq1 (dataSalasFilme)
+    hPutStr arq (formataParaEscrita filme)
     hClose arq
     hClose arq1
 
@@ -123,52 +126,54 @@ formataParaEscrita (c:cs) = filmeToString c ++ "\n" ++ formataParaEscrita cs
 
 
 ------------------------- Visualização de Filmes -------------------------
+getFilmesPuros :: [Filme]
+getFilmesPuros = (unsafePerformIO getFilmesEmLista :: [Filme])
 
-getFilmesemLista :: IO [Filme]
-getFilmesemLista = do
-    Filmes <- openFile "../arquivos/Filmes.csv" ReadMode
-    listaFilmes <- lines <$> hGetContents Filmes
-    hClose Filmes
+getFilmesEmLista :: IO [Filme]
+getFilmesEmLista = do
+    filmes <- openFile "../arquivos/Filmes.csv" ReadMode
+    listaFilmes <- lines <$> hGetContents filmes
+    hClose filmes
     return $ (converteEmLista listaFilmes)
 
 converteEmLista :: [String] -> [Filme]
 converteEmLista [] = []
-converteEmLista (Filme:lista) =
-    converteEmFilme (split Filme ',') : converteEmLista lista
+converteEmLista (filme:lista) =
+    converteEmFilme (split filme ',') : converteEmLista lista
 
 converteEmFilme :: [String] -> Filme
-converteEmFilme Filme = Filme id titulo preco salaFilme dataDuracao
+converteEmFilme filme = Filme id nome preco salasFilme dataDuracao
     where 
-        id = (read (Filme !! 0) :: Int)
-        titulo = Filme !! 1
-        preco = (read (Filme !! 2) :: Double)
-        salaFilme = fromIO(getsalaEmLista id)
-        dataDuracao = Filme !! 3
+        id = (read (filme !! 0) :: Int)
+        nome = filme !! 1
+        preco = (read (filme !! 2) :: Double)
+        salasFilme = fromIO(getSalasEmLista id)
+        dataDuracao = filme !! 3
 
 -- Converte IO em puro
 fromIO :: IO[String] -> [String]
 fromIO x = (unsafePerformIO x :: [String])
 
-------------------------- Visualização de sala -------------------------
+------------------------- Visualização de Salas -------------------------
 
-getsalaEmLista :: Int -> IO [String]
-getsalaEmLista id = do
-    sala <- openFile "../arquivos/salaFilme.csv" ReadMode
-    listasala <- lines <$> hGetContents sala
-    let salaFinal = converteSalaEmLista listasala
-    hClose sala
-    return $ (filtraSala id salaFinal)
+getSalasEmLista :: Int -> IO [String]
+getSalasEmLista id = do
+    salas <- openFile "../arquivos/SalasFilme.csv" ReadMode
+    listaSalas <- lines <$> hGetContents salas
+    let salasFinal = converteSalasEmLista listaSalas
+    hClose salas
+    return $ (filtraSala id salasFinal)
 
-converteSalaEmLista :: [String] -> [[String]]
-converteSalaEmLista [] = []
-converteSalaEmLista (sala:lista) =
-    (split sala ',') : converteSalaEmLista lista
+converteSalasEmLista :: [String] -> [[String]]
+converteSalasEmLista [] = []
+converteSalasEmLista (sala:lista) =
+    (split sala ',') : converteSalasEmLista lista
     
 filtraSala :: Int -> [[String]] -> [String]
 filtraSala _ [] = []
-filtraSala id (sala:sala)
+filtraSala id (sala:salas)
     | id == idCliente = tail sala
-    | otherwise = filtraSala id sala
+    | otherwise = filtraSala id salas
     where
         idCliente = read (head sala) :: Int
 ---------------------------------------------------------------------------------------
